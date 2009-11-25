c59388f94b923be1c0735c77b5121d6eb39a612e
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2009-11-25 15:25:18 +0530

    Added notifications for talks

--- apps/talk/models.py
+++ apps/talk/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
 from tagging.fields import TagField
 from django.contrib.auth.models import User
+if "notification" in settings.INSTALLED_APPS:
+    from notification import models as notification
+else:
+    notification = None
 
 class Topic(models.Model):
     name = models.CharField(max_length = 150)
@@ -28,3 +33,22 @@ class Talk(models.Model):
 
     def __unicode__(self):
         return "%s : %s" %(self.topic, self.title)
+
+def talk_added(sender, instance=None, **kwargs):
+    if notification and kwargs['created']:
+        users = [ii.user for ii in notification.NoticeSetting.objects.filter(notice_type__lable='talk_added')]
+        notification.send(users, "talk_added", {"descr": "Talk with title \"%s\" added" %instance.title})
+
+def talk_accepted(sender, instance=None, **kwargs):
+    if notification and not kwargs['created'] and instance.accepted:
+        users = [ii.user for ii in notification.NoticeSetting.objects.filter(notice_type__lable='talk_accepted')]
+        notification.send(users, "talk_accepted", {"descr": "Talk with title \"%s\" accepted" %instance.title)
+
+def talk_scheduled(sender, instance=None, **kwargs):
+    if notification and not kwargs['created'] and instance.scheduled:
+        users = [ii.user for ii in notification.NoticeSetting.objects.filter(notice_type__lable='talk_scheduled')]
+        notification.send(users, "talk_scheduled", {"descr": "Talk with title \"%s\" scheduled" %instance.title)
+
+models.signals.post_save.connect(talk_added,     sender=Talk)
+models.signals.post_save.connect(talk_accepted,  sender=Talk)
+models.signals.post_save.connect(talk_scheduled, sender=Talk)
