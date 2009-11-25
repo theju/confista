896170d9907bf1617d9930cfafb61a739012fff7
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2009-11-25 15:08:57 +0530

    Added ability to inform user whenever a deadline is created/modified

--- apps/deadline/models.py
+++ apps/deadline/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.conf import settings
+if "notification" in settings.INSTALLED_APPS:
+    from notification import models as notification
+else:
+    notification = None
 
 class Deadline(models.Model):
     code_name = models.CharField(max_length = 255, unique = True)
@@ -7,3 +12,9 @@ class Deadline(models.Model):
 
     def __unicode__(self):
         return "%s expires on %s" %(self.descr, self.expiry)
+
+def deadline_added(sender, instance, **kwargs):
+    if notification:
+        users = [ii.user for ii in notification.NoticeSetting.objects.filter(notice_type__label="deadlines_notification")]
+        notification.send(users, "deadlines_notification",{"descr": instance})
+models.signals.post_save.connect(deadline_added, sender=Deadline)
