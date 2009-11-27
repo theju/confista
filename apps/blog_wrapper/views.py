fb5007c3049dd47220cd7650522733f012d81327
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2009-11-27 12:55:43 +0530

    Added total blog feed and per user blog feed

--- apps/blog_wrapper/views.py
+++ apps/blog_wrapper/views.py
@@ -1,4 +1,6 @@
 from blog.views import new, edit, destroy
+from django.contrib.syndication.views import feed
+from blog_wrapper.feeds import AllBlogFeed, BlogUserFeed
 from django.contrib.auth.decorators import permission_required
 
 @permission_required("blog.add_post", login_url="/blog/not_authorized/")
@@ -12,3 +14,15 @@ def edit_post(request, **kwargs):
 @permission_required("blog.delete_post", login_url="/blog/not_authorized/")
 def destroy_post(request, **kwargs):
     return destroy(request, **kwargs)
+
+def feed_all(request):
+    url = 'feed/'
+    return feed(request, url, {
+        "feed": AllBlogFeed,
+    })
+
+def feed_user(request, username):
+    url = 'feed/%s' %username
+    return feed(request, url, {
+        "feed": BlogUserFeed,
+    })
