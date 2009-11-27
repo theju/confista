d0d77c29ab3446131b08948e4d6b9392f7f33950
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2009-11-27 14:04:41 +0530

    Added feed in urls.py

--- apps/talk/urls.py
+++ apps/talk/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^(?P<id>\d+)/$', "talk.views.list",     name="talk_list_id"),
     url(r'^all/$',         "talk.views.list",     name="talk_list_all"),
     url(r'^add/$',         "talk.views.add_talk", name="talk_add_talk"),
+    url(r'^feed/$',        "talk.views.feed_all", name="talk_feed_all"),
 )
